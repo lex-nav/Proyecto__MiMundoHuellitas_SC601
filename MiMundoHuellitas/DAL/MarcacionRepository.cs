110d0a2bc977666713097e9b120b42e664222dc9
SebasMB27 <163576383+SebasMB27@users.noreply.github.com>
2026-02-03 14:32:38 -0600

    Calculo de salario, marcacion de entrarda y salida automatico, regitro de horas extra y dobles automatico segun jordana de trabajo asignada

---
+++ MiMundoHuellitas/DAL/MarcacionRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TuProyecto.DAL
+{
+    public class MarcacionRepository
+    {
+        private readonly string _cn = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public void MarcarEntrada(int idUsuario)
+        {
+            EjecutarSP("dbo.usp_MH_MarcarEntrada", idUsuario);
+        }
+
+        public void MarcarSalida(int idUsuario)
+        {
+            EjecutarSP("dbo.usp_MH_MarcarSalida", idUsuario);
+        }
+
+        private void EjecutarSP(string spName, int idUsuario)
+        {
+            using (var conn = new SqlConnection(_cn))
+            using (var cmd = new SqlCommand(spName, conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdUsuario", idUsuario);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
