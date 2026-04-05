416ad15fa86f3db500c4918298f1c451e112770e
SebasMB27 <163576383+SebasMB27@users.noreply.github.com>
2026-04-04 22:59:15 -0600

    Listo, arreglado

--- MiMundoHuellitas/Models/ViewModels/Reportes/ReporteProductividadColaboradorVM.cs
+++ MiMundoHuellitas/Models/ViewModels/Reportes/ReporteProductividadColaboradorVM.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-namespace MiMundoHuellitas.Models.ViewModels
+namespace MiMundoHuellitas.Models.ViewModels.Reportes
 {
     public class ReporteProductividadColaboradorVM
     {
