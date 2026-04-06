af3e3a2decd4f1bc8a318b8147edf29241b889ce
Melany153 <114964598+Melany153@users.noreply.github.com>
2026-04-06 01:28:07 -0600

    Reporte Consumo y stock-Mel

---
+++ MiMundoHuellitas/Models/ViewModels/Reportes/ReporteConsumoVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiMundoHuellitas.Models.ViewModels.Reportes
+{
+    public class ReporteConsumoVM
+    {
+        public DateTime? Desde { get; set; }
+        public DateTime? Hasta { get; set; }
+        public string Texto { get; set; }
+
+        public int TotalServiciosConsumidos { get; set; }
+        public decimal TotalIngresos { get; set; }
+        public decimal PromedioPorServicio { get; set; }
+
+        public List<ReporteConsumoFilaVM> Filas { get; set; } = new List<ReporteConsumoFilaVM>();
+    }
+
+    public class ReporteConsumoFilaVM
+    {
+        public int IdServicio { get; set; }
+        public string NombreServicio { get; set; }
+        public int VecesConsumido { get; set; }
+        public int CantidadTotal { get; set; }
+        public decimal IngresoTotal { get; set; }
+        public decimal PrecioPromedio { get; set; }
+    }
+}
---
+++ MiMundoHuellitas/Models/ViewModels/Reportes/ReporteStockVM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MiMundoHuellitas.Models.ViewModels.Reportes
+{
+    public class ReporteStockVM
+    {
+        public string Categoria { get; set; }
+        public bool? SoloActivos { get; set; }
+        public string Texto { get; set; }
+
+        public int TotalProductos { get; set; }
+        public int ProductosSinStock { get; set; }
+        public int ProductosStockBajo { get; set; }
+        public int ProductosStockNormal { get; set; }
+
+        public IEnumerable<SelectListItem> Categorias { get; set; }
+
+        public List<ReporteStockFilaVM> Filas { get; set; } = new List<ReporteStockFilaVM>();
+    }
+
+    public class ReporteStockFilaVM
+    {
+        public int IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public string Categoria { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int StockActual { get; set; }
+        public string Estado { get; set; }
+        public bool Activo { get; set; }
+    }
+}
